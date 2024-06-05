4c53a977a58b62fb633f05efc9e96aef40a7dbcb
VivienIngrams <vivingrams@gmail.com>
2024-06-05 13:05:22 +0100

    Switch theme

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Link
- from 'next/link'
-const Navbar = () => {
-  return (
-    <div>
-        <Link href="https://www.youtube.com/watch?v=yAqgjSZ0PqY">tutorial</Link>
-    </div>
-  )
+import React from "react";
+import Link from "next/link";
+import ThemeSwitch from "./ThemeSwitch";
+{
+  /* <Link href="https://www.youtube.com/watch?v=yAqgjSZ0PqY" >tutorial</Link> */
 }
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <div className="mx-auto max-w-5xl px-6">
+      <div className="flex justify-between items-center h-16 w-full">
+        <Link href="/">
+          <div>Bombarda</div>
+        </Link>
+      <ThemeSwitch />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/utils/Provider.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+import { ThemeProvider } from 'next-themes'
+
+interface ProviderProps {
+    children: React.ReactNode
+}
+
+export const Provider = ({ children }: ProviderProps) => {
+    return (
+        <ThemeProvider attribute="class">
+            {children}
+        </ThemeProvider>
+    )
+}
